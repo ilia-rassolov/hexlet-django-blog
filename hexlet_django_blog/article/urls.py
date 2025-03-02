e6c7f4034456a83ba6169556e72387d164974fac
ilia <iliarassolov@gmail.com>
2025-03-02 22:25:38 +0400

    02/03/2025

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView
+
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    path('', IndexView.as_view(), name='index'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'Article',
-    })
+# def index(request):
+#     return render(request, 'index.html', context={
+#         'who': 'Article',
+#     })
+
+class IndexView(View):
+
+
+    template_name = "index.html"
+    initkwargs = {'template_name': 'about.html'}
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse()
+    #
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['who'] = 'Article'
+    #     return context
